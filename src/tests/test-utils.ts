9cee6580a0dce223824b2b83e61d713b3b712cea
peps1 <peps1@users.noreply.github.com>
2020-10-11 23:21:25 -0700

    adding some tests so it gh-actions don't fail

---
+++ src/tests/test-utils.ts
@@ -0,0 +1,38 @@
+
+import { expect } from 'chai';
+import * as assert from 'assert';
+import { describe, it } from 'mocha';
+// import sinon from 'sinon';
+
+import { formatSize, getLastDirectory, sleep } from '../utils';
+
+describe('formatSize', () => {
+  it('Should format bytes to MiB, GiB, TiB', () => {
+    expect(formatSize(9812391156851)).to.equal('8.92 TB');
+  });
+  it('Should properly show Byte values', () => {
+    expect(formatSize(1000)).to.equal('1000 B');
+  });
+});
+
+describe('getLastDirectory', () => {
+  it('Should return last folder of a path, when no trailing slash provided', () => {
+    expect(getLastDirectory('/home/user/folder/2ndfolder/lastfolder')).to.equal('lastfolder');
+  });
+  it('Should return last folder of a path, when trailing slash provided', () => {
+    expect(getLastDirectory('/home/user/folder/2ndfolder/lastfolder/')).to.equal('lastfolder');
+  });
+  it('Should return last folder, when root level folder', () => {
+    expect(getLastDirectory('/home')).to.equal('home');
+  });
+  it('Should return full path if last folder can\'t be determined', () => {
+    expect(getLastDirectory('/home/user//')).to.equal('/home/user//');
+  });
+});
+
+describe('sleep', () => {
+  it('Should sleep for specified amount', () => {
+    sleep(100);
+    assert.ok(true);
+  });
+})
